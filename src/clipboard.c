7544fdbe58fed9f63b5b0d63d862aaff58422ad3
Shuanglei Tao <tsl0922@gmail.com>
2024-02-05 10:25:33 +0800

    split clipboard code

---
+++ src/clipboard.c
@@ -0,0 +1,69 @@
+// Copyright (c) 2023-2024 tsl0922. All rights reserved.
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <windows.h>
+#include "mpv_talloc.h"
+#include "clipboard.h"
+
+// get clipboard text, always return utf8 string
+char *get_clipboard(plugin_ctx *ctx, void *talloc_ctx) {
+    if (!OpenClipboard(ctx->hwnd)) return NULL;
+
+    // try to get unicode text first
+    HANDLE hData = GetClipboardData(CF_UNICODETEXT);
+    char *ret = NULL;
+    if (hData != NULL) {
+        wchar_t *data = (wchar_t *)GlobalLock(hData);
+        if (data != NULL) {
+            ret = mp_to_utf8(talloc_ctx, data);
+            GlobalUnlock(hData);
+        }
+        goto done;
+    }
+
+    // try to get file drop list
+    hData = GetClipboardData(CF_HDROP);
+    if (hData != NULL) {
+        HDROP hDrop = (HDROP)GlobalLock(hData);
+        if (hDrop != NULL) {
+            int count = DragQueryFileW(hDrop, 0xFFFFFFFF, NULL, 0);
+            if (count > 0) {
+                void *tmp = talloc_new(NULL);
+                ret = talloc_strdup(talloc_ctx, "");
+                for (int i = 0; i < count; i++) {
+                    int len = DragQueryFileW(hDrop, i, NULL, 0);
+                    wchar_t *path_w = talloc_array(tmp, wchar_t, len + 1);
+                    if (DragQueryFileW(hDrop, i, path_w, len + 1)) {
+                        char *path = mp_to_utf8(tmp, path_w);
+                        ret = talloc_asprintf_append(ret, "%s\n", path);
+                    }
+                }
+                talloc_free(tmp);
+            }
+            GlobalUnlock(hData);
+        }
+    }
+
+done:
+    CloseClipboard();
+
+    return ret;
+}
+
+// set clipboard text, always convert to wide string
+void set_clipboard(plugin_ctx *ctx, const char *text) {
+    if (!OpenClipboard(ctx->hwnd)) return;
+    EmptyClipboard();
+
+    int len = MultiByteToWideChar(CP_UTF8, 0, text, -1, NULL, 0);
+    HGLOBAL hData = GlobalAlloc(GMEM_MOVEABLE, len * sizeof(wchar_t));
+    if (hData != NULL) {
+        wchar_t *data = (wchar_t *)GlobalLock(hData);
+        if (data != NULL) {
+            MultiByteToWideChar(CP_UTF8, 0, text, -1, data, len);
+            GlobalUnlock(hData);
+            SetClipboardData(CF_UNICODETEXT, hData);
+        }
+    }
+    CloseClipboard();
+}
--- src/menu.h
+++ src/menu.h
@@ -4,13 +4,10 @@
 #ifndef MPV_PLUGIN_MENU_H
 #define MPV_PLUGIN_MENU_H
 
-#include <windows.h>
-#include <mpv/client.h>
 #include "plugin.h"
 
 #define MENU_DATA_PROP "user-data/menu/items"
 
-void build_menu(void *talloc_ctx, HMENU hmenu, mpv_node *node);
 void update_menu(plugin_ctx *ctx, mpv_node *node);
 void show_menu(plugin_ctx *ctx, POINT *pt);
 void handle_menu(plugin_ctx *ctx, UINT id);
